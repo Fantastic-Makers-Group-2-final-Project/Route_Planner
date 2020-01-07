edbfde1c5491f647d862b38528d9c5963c85359a
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-7.local>
2020-01-07 15:55:10 +0000

    front end statement updated
    Co-authored-by: Alex Clifford <aclifford336@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('RouteAround - the route planner for urban runners, leisurely strollers and dogwalkers'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
