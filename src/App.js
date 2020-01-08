117ca426001a64687c6384316b1af3f5a516579e
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-7.local>
2020-01-08 11:19:35 +0000

    homepage on heroku updated for the sake of demo
    Co-authored-by: Alex Clifford <aclifford336@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import logo from './logo.svg';
-// import './App.css';
-
-// const express = require('express');
-// const port = process.env.Port || 3000;
-// const app = express();
-// app.get('/', function (req, res) {
-//   res.send(JSON.stringify({'RouteAround - the route planner for urban runners, leisurely strollers and dogwalkers'}));
-// });
-
-// app.listen(port, function () {
-//   console.log(`Example app listening on port ${port}!`);
-// });
+import './app.css';
 
 var createError = require('http-errors');
 var express = require('express');
@@ -23,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.status(200).json({
-    "thing": "somethingElse"
+    "This is where our RouteAround web app will go :)"
   })
 });
 app.post('/', (req, res) => {
