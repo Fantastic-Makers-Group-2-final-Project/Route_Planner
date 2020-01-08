ffe46b7da2301a24646eeb77a36fa8319b01c571
ellieSMASH <eliseaston@gmail.com>
2020-01-08 18:18:22 +0000

    refactor: findCoordinates now delegates to separate findWaypoint functions

--- src/find_coordinates.js
+++ src/find_coordinates.js
@@ -1,5 +1,22 @@
 function findCoordinates([lat,long], distance) {
-  return [{"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}]
-}
+  // return [{"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}]
+  newCoordinates = [];
+  findWaypointA([lat,long], distance);
+  findWaypointB([lat,long], distance);
+  findWaypointC([lat,long], distance);
+  return newCoordinates;
+};
+
+function findWaypointA([lat,long], distance) {
+  newCoordinates.push({"latitude": lat, "longitude": long});
+};
+
+function findWaypointB([lat,long], distance) {
+  newCoordinates.push({"latitude": lat, "longitude": long});
+};
+
+function findWaypointC([lat,long], distance) {
+  newCoordinates.push({"latitude": lat, "longitude": long});
+};
 
 module.exports = findCoordinates;
