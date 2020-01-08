40447911d38df98f09e618814a986bf6def2a716
Student <student@Makerss-MacBook-Pro-8.local>
2020-01-08 12:17:05 +0000

    switched from mocha to jest test suite
    Co-Authered-By: Ingrid Bjarman <bjarmaningrid@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./src/App');
 app.listen(process.env.PORT || 5678, () => {
-console.log('example app listening on port 5678')
+  console.log('example app listening on port 5678')
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// var styleSheet = require('src/App.css');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
