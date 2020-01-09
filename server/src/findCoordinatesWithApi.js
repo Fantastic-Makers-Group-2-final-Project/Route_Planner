ad079b6eedcd28c4883168aff3d4ad30a1b87810
ellieSMASH <eliseaston@gmail.com>
2020-01-09 17:48:33 +0000

    import spherical-geometry-js library. Can now calculate new coordinate on back-end

--- server/src/findCoordinatesWithApi.js
+++ server/src/findCoordinatesWithApi.js
@@ -1,7 +1,12 @@
+const geometry = require('spherical-geometry-js');
+
 function findCoordinates() {
+  var startPoint = new geometry.LatLng(0,0);
+  var distance = 0;
+  var heading = 0;
 
-  return [{"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}, {"latitude": 0, "longitude": 0}];
-
+  var endPoint = geometry.computeOffset(startPoint, distance, heading);
+  return endPoint.toJSON();
 };
 
 module.exports = findCoordinates;
