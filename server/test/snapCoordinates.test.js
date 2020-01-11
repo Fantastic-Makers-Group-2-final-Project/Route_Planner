b039f899f884f1cf3e33e1f8bc2239eb7208f571
Robert Woolley <rob@robertwoolley.co.uk>
2020-01-11 17:57:30 +0000

    All tests passing. Removed scaffolding; cleaned up.
    Co-authored-by: Elise Aston <eliseaston@gmail.com>

--- server/test/snapCoordinates.test.js
+++ server/test/snapCoordinates.test.js
@@ -1,11 +1,13 @@
-// const snapCoordinates = require('../src/snapCoordinates.js')
-//
-// test('the lat lng values for a given coordinate remain the same', () => {
-//   expect(snapCoordinates([51.517886201263657,-0.076167271243431728])).toEqual([51.517886201263657,-0.076167271243431728]);
-// });
-//
-// test('the lat lng values of a location near Makers snaps to Makers', () => {
-//   return snapCoordinates([51.5178767,-0.0762007]).then(data => {
-//     expect(data).toEqual([51.517886201263657,-0.076167271243431728]);
-//   });
-// });
+const snapCoordinates = require('../src/snapCoordinates.js')
+
+test('the lat lng values for a given coordinate remain the same', () => {
+  return snapCoordinates([[51.517886201263657,-0.07616727124343367]]).then(data => {
+    expect(data).toEqual([{"latitude":51.517886201263657, "longitude": -0.07616727124343367}, {"latitude":51.517886201263657, "longitude": -0.07616727124343367}]);
+  });
+});
+
+test('the lat lng values of a location near Makers snaps to Makers',  () => {
+  return snapCoordinates([[51.5178767,-0.0762007]]).then(data => {
+    expect(data).toEqual([{"latitude": 51.51788620126366, "longitude": -0.07616727124343173}, {"latitude": 51.51788620126366, "longitude": -0.07616727124343173}]);
+  });
+});
