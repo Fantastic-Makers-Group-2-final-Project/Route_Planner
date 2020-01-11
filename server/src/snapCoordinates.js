2b8e5fc28374a91ab21e68a4d7e70563010d82ef
Robert Woolley <rob@robertwoolley.co.uk>
2020-01-11 16:54:27 +0000

    Now with working function to snap coordinates to roads
    Co-authored-by: Elise Aston <eliseaston@gmail.com>

--- server/src/snapCoordinates.js
+++ server/src/snapCoordinates.js
@@ -3,31 +3,27 @@ const googleMapsClient = require('@google/maps').createClient({
   Promise: Promise
 })
 
-// async function snapCoordinates([lat, lng]) {
-//   googleMapsClient.snapToRoads({
-//     path: [
-//       [lat, lng]
-//     ],
-//   }).asPromise().then((response) => {
-//     console.log(response.json.snappedPoints[0].location);
-//     return response.json.snappedPoints[0].location;
-//   })
-//   .catch(err => err);
-// }
-//
-// module.exports = snapCoordinates;
+const coordsObjectToArray = require('./coordsObjectToArray.js')
 
-
-async function snapCoordinates([lat, lng]) {
+async function snapCoordinates(input) {
   googleMapsClient.snapToRoads({
-    path: [
-      [lat, lng]
-    ],
+    path: input,
   }).asPromise().then((response) => {
-    console.log(response.json.snappedPoints[0].location);
-    return response.json.snappedPoints[0].location;
+    console.log(response.json.snappedPoints[1].location);
+    output = []
+    for (i = 0; i < response.json.snappedPoints.length; i++){
+      output.push(response.json.snappedPoints[i].location)
+    console.log(i)}
+    output.push(response.json.snappedPoints[0].location)
+    console.log(output)
+    return output;
   })
   .catch(err => err);
 }
 
 module.exports = snapCoordinates;
+
+// let locations = coordsObjectToArray([ { lat: 51.51787669999999, lng: -0.07620069999995849 },
+//   { lat: 51.52581606811841, lng: -0.06343865245844427 },
+//   { lat: 51.5337592191676, lng: -0.07620069999995849 },
+//   { lat: 51.52581606811841, lng: -0.08896274754147271 }])
