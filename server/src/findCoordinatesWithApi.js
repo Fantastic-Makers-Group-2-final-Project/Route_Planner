13f6cbe115778b23492b11ca736439abb231d321
ellieSMASH <eliseaston@gmail.com>
2020-01-13 09:45:20 +0000

    reformat findCoordinatesWithApi function to require startPoint as an object rather than array. All tests passing

--- server/src/findCoordinatesWithApi.js
+++ server/src/findCoordinatesWithApi.js
@@ -1,8 +1,6 @@
 const geometry = require('spherical-geometry-js');
 
-function findCoordinates([lat,lng], distance) {
-
-  var startPoint = new geometry.LatLng(lat,lng);
+function findCoordinates(startPoint, distance) {
 
   var startPointFormatted = geometry.computeOffset(startPoint, 0, 0);
   var wayPointA = geometry.computeOffset(startPoint, distance/4, 45);
