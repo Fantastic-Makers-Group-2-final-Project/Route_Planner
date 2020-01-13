167dd97447cff03152b767e4e901be32a27768a4
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-7.local>
2020-01-13 22:13:49 +0000

    Mega Function now works, and returns lat/lng objects

--- src/findAndSnapCoords.js
+++ src/findAndSnapCoords.js
@@ -2,18 +2,12 @@ const findCoordinatesWithApi = require('./findCoordinatesWithApi');
 const coordsObjectToArray = require('./coordsObjectToArray');
 const snapCoordinates = require('./snapCoordinates');
 
-function findAndSnapCoords(startPoint, distance) {
+async function findAndSnapCoords(startPoint, distance) {
   var distanceInMetres = distance * 1000;
-
   var newCoordinates = findCoordinatesWithApi(startPoint, distanceInMetres);
-
   var convertedCoords = coordsObjectToArray(newCoordinates);
-
-  var snappedCoords = snapCoordinates(convertedCoords).then(data => {
-    return data;
-  });
-  return snappedCoords;
-
+  
+  return await snapCoordinates(convertedCoords);
 }
 
 module.exports = findAndSnapCoords;
