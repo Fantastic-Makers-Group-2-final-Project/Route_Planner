e2ebb723aeda86dd52304e648e3d2b13c12ddb02
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-7.local>
2020-01-13 11:45:51 +0000

    Minor tweaks to start script, heroku deployment attempt 2.0
    co-authored-by: Alex Clifford <aclifford336@gmail.com>

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const routes = require('./routes/routes.js')(app);
+const routes = require('../routes/routes.js')(app);
 
 const server = app.listen(3001, () => {
   console.log('listening on port %s...', server.address().port);
