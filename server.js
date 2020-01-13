154166a2ec291ce6003c1946069b12cc4e1871a1
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-7.local>
2020-01-13 14:08:48 +0000

    CORS library required to try to work around CORS error message 2.0
    co-authored-by: Alex Clifford <aclifford336@gmail.com>

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+const whitelist = ['https://routearound.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOptions));
 
 const routes = require('./routes/routes.js')(app);
 
