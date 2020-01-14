e48b06cf1703901827a7f15142f94f980ea75ba0
ellieSMASH <eliseaston@gmail.com>
2020-01-14 23:01:18 +0000

    test findCoordinates function. Returns an array of 8 coordinates that plot a 5km octagonal route

--- src/findCoordinatesWithApi.js
+++ src/findCoordinatesWithApi.js
@@ -1,22 +1,5 @@
 const geometry = require('spherical-geometry-js');
 
-// function findCoordinates(startPoint, distance) {
-//
-//   var distanceInMetres = distance * 1000;
-//
-//   bearings = [[45, 0, 315], [135, 90, 45], [225, 180, 135], [315, 270, 225]].sample
-//
-//   var startPointFormatted = geometry.computeOffset(startPoint, 0, 0);
-//   var wayPointA = geometry.computeOffset(startPoint, distanceInMetres/4, bearings[0]);
-//   var wayPointB = geometry.computeOffset(startPoint, distanceInMetres/2.828, bearings[1]);
-//   var wayPointC = geometry.computeOffset(startPoint, distanceInMetres/4, bearings[2]);
-//
-//   routeCoordinates = [startPointFormatted.toJSON(), wayPointA.toJSON(), wayPointB.toJSON(), wayPointC.toJSON()];
-//   return routeCoordinates;
-// };
-//
-// module.exports = findCoordinates;
-
 function findCoordinates(startPoint, distance) {
 
   var distanceInMetres = distance * 1000;
