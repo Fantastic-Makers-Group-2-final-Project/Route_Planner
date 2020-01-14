c7fd7187e244b7606ca5cf5d11cc80670875feac
ellieSMASH <eliseaston@gmail.com>
2020-01-14 12:44:02 +0000

    update routes.js to call findCoordinates instead of findAndSnapCoordinates. Update findCoordinates function. Delete findAndSnapCoordinates.
    Co-authored-by: Ingrid Bjarman <bjarmaningrid@gmail.com>

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
-const findAndSnapCoordinates = require('../src/findAndSnapCoords')
+const findCoordinates = require('../src/findCoordinatesWithApi')
 
 const appRouter = (app) => {
   app.get('/heartbeat', (req, res) => {
     res.send("OK");
   })
 
-  app.post('/generate-waypoint-coordinates', async (req, res) => {
-    let coordinates = await findAndSnapCoordinates(req.body.coordinates,req.body.distance);
+  app.post('/generate-waypoint-coordinates', (req, res) => {
+    let coordinates = findCoordinates(req.body.coordinates,req.body.distance);
     res.send(coordinates);
   })
 };
