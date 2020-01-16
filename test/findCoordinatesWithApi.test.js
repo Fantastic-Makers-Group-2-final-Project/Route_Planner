23ba29d298edb080e339173c1ded67698b694566
ellieSMASH <eliseaston@gmail.com>
2020-01-16 17:27:19 +0000

    seed random in test to always return north route. Tests now passing.
    Co-authored-by: Robert Woolley <rob@robertwoolley.co.uk>

--- test/findCoordinatesWithApi.test.js
+++ test/findCoordinatesWithApi.test.js
@@ -6,5 +6,8 @@ test('it returns 4 new coordinates of 0,0 when start point is 0,0 and distance i
 });
 
 test('it returns the NORTH bearings for a 5K route around Makers', () => {
+  const mockMath = Object.create(global.Math);
+  mockMath.random = () => 0;
+  global.Math = mockMath;
   expect(findCoordinates({lat: 51.5178767, lng: -0.0762007}, 5)).toEqual([{ lat: 51.51787669999999, lng: -0.07620069999995849 }, { lat: 51.523789952511784, lng: -0.06669614356542297 }, { lat: 51.52970575928739, lng: -0.07620069999995849 }, { lat: 51.523789952511784, lng: -0.085705256434494 }]);
 });
